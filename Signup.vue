1f5b16d2bd7a6ef13aa8eb40b4d428be0c6b4416
matejazivko <mateja.zivko100@gmail.com>
2024-08-22 22:18:35 +0200

    Add files via upload

--- Signup.vue
+++ Signup.vue
@@ -30,6 +30,11 @@
     <input type="password" class="form-control"
     id="exampleInputPassword1" placeholder="Password" />
     </div>
+    <div class="form-group">
+    <label for="exampleInputPassword1">Password Confirmation</label>
+    <input type="password" class="form-control"
+    id="exampleInputPassword1" placeholder="Password" />
+    </div>
     <button type="submit" class="btn btn-primary">Submit</button>
     </form>
     </div>
