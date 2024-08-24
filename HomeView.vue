a83faa6dd6058a19dc826c2df09753962a93b67a
matejazivko <mateja.zivko100@gmail.com>
2024-08-24 22:47:57 +0200

    Add files via upload

--- HomeView.vue
+++ HomeView.vue
@@ -1,20 +1,36 @@
 <template>
-  <div class="row">
-  <div class="col-4"></div>
+  <div class="container text-center">
+    <div class="row">
+    <div class="col-4">
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <img class="card-img-tops" src="@/assets/Kuca_za_odmor_Una.jpg" weight="120" height="150">
+    <p class="card-text" align="left"><b>Kuća za odmor Una</b> okružena zelenilom pružit će vam idealan odmor. Kuća ima bazen, terasu, tri spavaće sobe, kuhinju i kupaonicu. Kapacitet 6 osoba.</p>
+    <a href="#" class="card-link">Komentari</a><br>
+    <a href="#" class="card-link">Kontakt</a>
+  </div>
+</div>
+  </div>
   <div class="col-4">
     <div class="card" style="width: 18rem;">
   <div class="card-body">
-    <img class="card-img-tops" src="@/assets/Kuca_za_odmor_Una.jpg" weight="150" height="200">
-    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
+    <img class="card-img-tops" src="@/assets/Kuca_za_odmor_Mia.jpg" weight="20" height="150">
+    <p class="card-text" align="left"><b>Kuća za odmor Mia</b> s pogledom na more. U kući se nalaze dvije spavaće sobe i jedna kupaonica, što kuću čini idealnom za odmor 4 osobe.</p>
+    <a href="#" class="card-link">Komentari</a><br>
+    <a href="#" class="card-link">Kontakt</a>
   </div>
-</div>
-
+  </div>
   </div>
   <div class="col-4">
-  ... ovdje ide desni stupac!
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <img class="card-img-tops" src="@/assets/Kuca_za_odmor_Paula.jpg" height="130">
+    <p class="card-text" align="left"><b>Kuća za odmor Paula</b> u prirodi. Idealna za goste koji traže odmak od gradske vreve i gužve. Ima jednu spavaću sobu, kuhinju, kupaonicu i bazen. Kapacitet max. 3 osobe. Pet friendly.</p>
+    <a href="#" class="card-link">Komentari</a><br>
+    <a href="#" class="card-link">Kontakt</a>
+  </div>
+  </div>
+  </div>
   </div>
   </div>
   </template>
