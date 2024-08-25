48a2df7c14f723ab011fd2f5b5248c4b6f639ed9
matejazivko <mateja.zivko100@gmail.com>
2024-08-25 19:24:14 +0200

    Add files via upload

---
+++ Home.vue
@@ -0,0 +1,36 @@
+<template>
+ <div class="row">
+<div class="col-2">
+ 
+</div>
+<div class="col-8">
+  <house-card v-for="card in cards" :key="card" :info="card"/>
+  
+</div>
+<div class="col-2">
+
+</div>
+</div>
+  </template>
+
+<script>
+// @ is an alias to /src
+import HouseCard from '@/components/HouseCard.vue';
+let cards = []
+cards = [
+  {url: "https://picsum.photos/200/300", title: "opis", komentar: "komentari", kontakt: "kontakt"},
+  {url: "@/assets/Kuca_za_odmor_Mia.jpg", title: "opis", komentar: "komentari", kontakt: "kontakt"},
+  {url: "@/assets/Kuca_za_odmor_Paula.jpg", title: "opis", komentar: "komentari", kontakt: "kontakt"}
+]
+export default {
+  name: 'Home',
+  data: function() {
+    return{
+      cards: cards
+    }; 
+  },
+  components: {
+    HouseCard,
+  },
+};
+</script>
---
+++ HouseCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card-body p-0" style="width: 18rem;">
+  <img :src="info.url" class="card-img-top" alt="slika">
+  <div class="card-body">
+   <p class="card-text">{{ info.title }}</p>
+    <a href="#" class="btn btn-primary">{{ info.komentar }}</a><br><br>
+    <a href="#" class="btn btn-primary">{{ info.kontakt }}</a>
+  </div>
+</div>
+  
+</template>
+
+<script>
+export default {
+  props: ["info"],
+  name: 'HouseCard',
+  
+}
+</script>
+
+
